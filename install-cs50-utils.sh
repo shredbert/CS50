59e29c51a7fe95070f3b2925ac38dea73d35fc0c
rjadie <rob.adie@gmail.com>
2022-07-03 11:23:19 -0600

    Start rvw 1; add CS50 Python stuff to install script

--- install-cs50-utils.sh
+++ install-cs50-utils.sh
@@ -5,11 +5,15 @@ curl -s https://packagecloud.io/install/repositories/cs50/repo/script.deb.sh | s
 sudo apt install libcs50
 
 # style50
-sudo pip install style50
+pip install style50
 # Artistic Style 3.0 (style50 dependency)
 sudo add-apt-repository ppa:cs50/ppa
 sudo apt-get update
 sudo apt-get install astyle
 
 # check50
-sudo pip install check50
+pip install check50
+
+# CS50 Python lib & dependencies
+pip install cs50
+pip install pillow
---
+++ week-6/lectures/rvw-1/img-blur.py
@@ -0,0 +1,5 @@
+from PIL import Image, ImageFilter
+
+before = Image.open("../assets/test.bmp")
+after = before.filter(ImageFilter.BoxBlur(10))
+after.save("../assets/out-blur.bmp")
