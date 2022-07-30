77f9be390e0b101041b080a05b7f564216fe4fbb
rjadie <rob.adie@gmail.com>
2022-07-30 11:11:54 -0600

    Fin readability

--- week-6/sentimental-readability/readability.py
+++ week-6/sentimental-readability/readability.py
@@ -6,48 +6,36 @@ import re
 def main():
     # Get string of text from user for analysis
     text = get_string("Text: ")
-    # TEST
-    print(text)
 
     # Calculate num of letters (A-Z/a-z)
     letters = len(re.findall(r"[A-Za-z]", text))
 
     # Calculate num of words (any sequence of chars separated by spaces on
-    # EITHER END)
-    words = re.findall(r"\W", text)
-    print(words)
-    # words = len(re.findall(r"(\W+)", text))
+    # either end)
+    words = len(re.findall(r"\s*\S+\s*", text))
 
     # Calculate num of sentences (.!?)
     sentences = len(re.findall(r"(\w*)[.!?]", text))
 
-    # # TODO: Calculate avg letters per 100 words
-    # avg_letters = letters / words * 100
+    # Calculate avg letters per 100 words
+    avg_letters = letters / words * 100
 
-    # # TODO: Calculate avg sentences per 100 words
-    # avg_sentences = sentences / words * 100
+    # Calculate avg sentences per 100 words
+    avg_sentences = sentences / words * 100
 
-    # # TODO: Use Coleman-Liau formula to calculate reading level of input text
-    # reading_level = 0.0588 * avg_letters - 0.296 * avg_sentences - 15.8
+    # Use Coleman-Liau formula to calculate reading level of input text
+    reading_level = 0.0588 * avg_letters - 0.296 * avg_sentences - 15.8
 
-    # # TODO: Print grade level
-    # # TODO: If < 1, print "Before Grade 1"
-    # if reading_level < 1:
-    #     print("Before Grade 1")
-    # # TODO: If 16+, print "16+" -- senior undergraduate reading level
-    # elif reading_level >= 16:
-    #     print("Grade 16+")
-    # # TODO: Else round result to whole num for output
-    # else:
-    #     print(f"Grade {reading_level:.0f}")
-
-    # # TEST
-    # print(f"Letters: {letters}")
-    # print(f"Words: {words}")
-    # print(f"Sentences: {sentences}")
-    # print(f"Avg letters / 100 words: {avg_letters}")
-    # print(f"Avg sentences / 100 words: {avg_sentences}")
-    # print()
+    # Print grade level
+    # If < 1, print "Before Grade 1"
+    if reading_level < 1:
+        print("Before Grade 1")
+    # If 16+, print "16+" -- senior undergraduate reading level
+    elif reading_level >= 16:
+        print("Grade 16+")
+    # Else round result to whole num for output
+    else:
+        print(f"Grade {reading_level:.0f}")
 
 
 if __name__ == "__main__":
--- week-6/sentimental-readability/tests.sh
+++ week-6/sentimental-readability/tests.sh
@@ -1,52 +1,52 @@
 #!/usr/bin/env bash
 
-# echo "***Before Grade 1***"
-# ./readability.py << EOF
-# One fish. Two fish. Red fish. Blue fish.
-# EOF
+echo "***Before Grade 1***"
+./readability.py << EOF
+One fish. Two fish. Red fish. Blue fish.
+EOF
 
-# echo "***Grade 2***"
-# ./readability.py << EOF
-# Would you like them here or there? I would not like them here or there. I would not like them anywhere.
-# EOF
+echo "***Grade 2***"
+./readability.py << EOF
+Would you like them here or there? I would not like them here or there. I would not like them anywhere.
+EOF
 
-# echo "***Grade 3***"
-# ./readability.py << EOF
-# Congratulations! Today is your day. You're off to Great Places! You're off and away!
-# EOF
+echo "***Grade 3***"
+./readability.py << EOF
+Congratulations! Today is your day. You're off to Great Places! You're off and away!
+EOF
 
-# echo "***Grade 5***"
-# ./readability.py << EOF
-# Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard.
-# EOF
+echo "***Grade 5***"
+./readability.py << EOF
+Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard.
+EOF
 
 echo "***Grade 7***"
 ./readability.py << EOF
 In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.
 EOF
 
-# echo "***Grade 8***"
-# ./readability.py << EOF
-# Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what is the use of a book," thought Alice "without pictures or conversation?"
-# EOF
+echo "***Grade 8***"
+./readability.py << EOF
+Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what is the use of a book," thought Alice "without pictures or conversation?"
+EOF
 
 echo "***Grade 8***"
 ./readability.py << EOF
 When he was nearly thirteen, my brother Jem got his arm badly broken at the elbow. When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury. His left arm was somewhat shorter than his right; when he stood or walked, the back of his hand was at right angles to his body, his thumb parallel to his thigh.
 EOF
 
-# echo "***Grade 9***"
-# ./readability.py << EOF
-# There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy.
-# EOF
+echo "***Grade 9***"
+./readability.py << EOF
+There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy.
+EOF
 
-# echo "***Grade 10***"
-# ./readability.py << EOF
-# It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.
-# EOF
+echo "***Grade 10***"
+./readability.py << EOF
+It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him.
+EOF
 
-# echo "***Grade 16+***"
-# ./readability.py << EOF
-# A large class of computational problems involve the determination of properties of graphs, digraphs, integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains.
-# EOF
+echo "***Grade 16+***"
+./readability.py << EOF
+A large class of computational problems involve the determination of properties of graphs, digraphs, integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains.
+EOF
 
