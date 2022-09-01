b70a2554190756a6d0fd374113ec127f7aeebe8a
rjadie <rob.adie@gmail.com>
2022-09-01 17:10:31 -0600

    Erpie derpies

--- week-7/rvw-1/movies/12.sql
+++ week-7/rvw-1/movies/12.sql
@@ -1,6 +1,15 @@
--- SELECT movies.title FROM movies
+-- SELECT title FROM movies
+-- JOIN stars ON movies.id = stars.movie_id
+-- -- JOIN people ON stars.movie_id = people.id
+-- WHERE EXISTS (
+--     SELECT person_id FROM stars
+--     JOIN people ON stars.person_id = people.id
+--     WHERE stars.movie_id = movies.id
+--     AND people.name IN ('Johnny Depp', 'Helena Bonham Carter')
+-- );
+
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE people.name = 'Johnny Depp'
--- WHERE people.name = 'Johnny Depp' AND people.name = 'Helena Bonham Carter';
+WHERE EXISTS (
+    SELECT 
+);
