51b29bd15c8ff934046a55f51f243d4f09bd8520
rjadie <rob.adie@gmail.com>
2022-10-22 15:11:56 -0600

    Fin lecture notes rvw

--- week-9/rvw-1/lectures/shows/app.py
+++ week-9/rvw-1/lectures/shows/app.py
@@ -1,11 +1,24 @@
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request
+from flask import Flask, jsonify, redirect, render_template, request
 
 app = Flask(__name__)
-
 db = SQL("sqlite:///shows.db")
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+    if query:
+        shows = db.execute(
+            "SELECT * FROM shows WHERE title LIKE ?",
+            f"%{query}%"
+        )
+    else:
+        shows = []
+    # print(shows)
+    return jsonify(shows)
