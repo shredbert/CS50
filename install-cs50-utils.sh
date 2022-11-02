c3bdcfbdbefad7dc8286b780cf3eea364d8651da
Bert <guitarwhore@gmail.com>
2022-11-02 10:30:44 -0600

    Cleanup

--- install-cs50-utils.sh
+++ install-cs50-utils.sh
@@ -1,19 +1,17 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # lib50
 curl -s https://packagecloud.io/install/repositories/cs50/repo/script.deb.sh | sudo bash
 sudo apt install libcs50
 
-# style50
-pip install style50
 # Artistic Style 3.0 (style50 dependency)
 sudo add-apt-repository ppa:cs50/ppa
 sudo apt-get update
 sudo apt-get install astyle
 
-# check50
-pip install check50
-
 # CS50 Python lib & dependencies
 pip install cs50
+pip install style50
+pip install check50
 pip install pillow
+pip install flask flask-session flask-mail
