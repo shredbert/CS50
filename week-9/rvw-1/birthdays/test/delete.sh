edefb19a4434b2a590f9bdf6dd8b228d10a41eb3
rjadie <rob.adie@gmail.com>
2022-11-04 22:04:49 -0600

    Erpderts -- diagnosing sturf

--- week-9/rvw-1/birthdays/test/delete.sh
+++ week-9/rvw-1/birthdays/test/delete.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -X DELETE http://127.0.0.1:5000/?id=5
+curl -X DELETE -L http://127.0.0.1:5000/?id=1
--- week-9/rvw-1/birthdays/test/get.sh
+++ week-9/rvw-1/birthdays/test/get.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl http://127.0.0.1:5000
+curl -L http://127.0.0.1:5000
