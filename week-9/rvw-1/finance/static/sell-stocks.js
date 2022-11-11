5115e5fe9bdbda47d4839861aa315b1d89599a10
Bert <guitarwhore@gmail.com>
2022-11-11 11:46:20 -0700

    Work on sell interactivity with select element

--- week-9/rvw-1/finance/static/sell-stocks.js
+++ week-9/rvw-1/finance/static/sell-stocks.js
@@ -1,5 +1,13 @@
 // Script for Sell Stocks page
 (() => {
-    // Dynamically render # of shares available after selecting stock to sell
-    console.log('script loaded for sell stonks');
+    document.querySelector('#stockSelector').addEventListener('click', (e) => {
+        let prevIdx = e.target.selectedIndex;
+        if (e.target.selectedIndex > 0 || e.target.selectedIndex !== prevIdx) {
+            console.log(e.target.selectedIndex);
+            // // Dynamically render # of shares available after selecting stock to sell
+            // const sharesOwnedTemplate = document.querySelector('#totalSharesOwnedTemplate');
+            // const sharesOwnedNode = sharesOwnedTemplate.cloneNode();
+            // console.log(sharesOwnedNode);
+        }
+    });
 })();
