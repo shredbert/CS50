cf899bcab2d180df034c51b4feb206e8ad41f20c
Bert <guitarwhore@gmail.com>
2022-11-15 11:53:37 -0700

    Add check for transactions types -- in "buy" & "sell"

--- week-9/rvw-1/finance/scripts/create-stock-transactions-table.sql
+++ week-9/rvw-1/finance/scripts/create-stock-transactions-table.sql
@@ -5,7 +5,7 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE stock_transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     user_id INTEGER NOT NULL,
-    transaction_type TEXT NOT NULL,
+    transaction_type TEXT NOT NULL CHECK(transaction_type IN ("buy", "sell")),
     stock_symbol TEXT NOT NULL,
     number_of_shares INTEGER NOT NULL CHECK(number_of_shares > 0),
     stock_price REAL NOT NULL CHECK(stock_price > 0),
