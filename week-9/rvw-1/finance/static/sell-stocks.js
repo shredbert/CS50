4621fdc593e6687d3f8a2f52eb3718eb504dbfdd
Bert <guitarwhore@gmail.com>
2022-11-15 11:10:43 -0700

    Get JS working for rendering # of stocks owned

--- week-9/rvw-1/finance/static/sell-stocks.js
+++ week-9/rvw-1/finance/static/sell-stocks.js
@@ -1,13 +1,14 @@
 // Script for Sell Stocks page
 (() => {
-    document.querySelector('#stockSelector').addEventListener('click', (e) => {
-        let prevIdx = e.target.selectedIndex;
-        if (e.target.selectedIndex > 0 || e.target.selectedIndex !== prevIdx) {
-            console.log(e.target.selectedIndex);
-            // // Dynamically render # of shares available after selecting stock to sell
-            // const sharesOwnedTemplate = document.querySelector('#totalSharesOwnedTemplate');
-            // const sharesOwnedNode = sharesOwnedTemplate.cloneNode();
-            // console.log(sharesOwnedNode);
-        }
+    // Display form group for shares owned -- populated with JS so hidden if JS
+    // not enabled
+    document.querySelector('#sharesOwnedGroup').removeAttribute('hidden');
+
+    // Populate field when different stock symbols are selected
+    document.querySelector('#stockSelector').addEventListener('change', (e) => {
+        const stockSymbol = e.target.value;
+        const sharesOwned = e.target
+            .querySelector(`option[value=${stockSymbol}]`).dataset.totalShares;
+        document.querySelector('#sharesOwned').value = sharesOwned;
     });
 })();
